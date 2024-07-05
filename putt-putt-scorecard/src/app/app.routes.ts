d9e3aa7a89b0af77579e1689d8c15471c6a4db6d
NicholasBobich <Nbobich2015@gmail.com>
2024-07-05 12:17:44 -0400

    Added routing for all menu buttons

--- putt-putt-scorecard/src/app/app.routes.ts
+++ putt-putt-scorecard/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PlayComponent } from './components/play/play.component';
+import { AddComponent } from './components/add/add.component';
+import { HistoryComponent } from './components/history/history.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'play', component: PlayComponent },
+    { path: 'add', component: AddComponent },
+    { path: 'history', component: HistoryComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: HomeComponent }    // TODO: Create page not found component
 ];
--- putt-putt-scorecard/src/app/components/home/home.component.ts
+++ putt-putt-scorecard/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
