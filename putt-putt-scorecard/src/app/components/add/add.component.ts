4719a040ecdba3d5f72383f77b4e1144a217ff41
NicholasBobich <Nbobich2015@gmail.com>
2024-07-16 17:56:40 -0400

    Adding UI for adding players and updating CSS id/class names

--- putt-putt-scorecard/src/app/components/add/add.component.ts
+++ putt-putt-scorecard/src/app/components/add/add.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+interface Option {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, NgFor, NgIf, NgSwitch, NgSwitchCase],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css'
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+  options: Option[] = [
+    { id: 1, name: "PLAYER" },
+    { id: 2, name: "COURSE" }
+  ];
+  selectedOption: Option | null = null;
+  showOptions = true;
 
+  @Output() selectedOptionChange = new EventEmitter<Option>();
+
+  ngOnInit(): void {
+    
+  }
+
+  onOptionSelected(option: Option) {
+    this.selectedOption = option;
+    this.showOptions = false;
+    this.selectedOptionChange.emit(option);
+  }
 }
